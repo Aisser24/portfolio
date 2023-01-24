aed7540b153cce3a0b7de3dfd5e880db657b37ef
Aisser24 <jonathanguger@gmail.com>
2023-01-24 22:03:55 +0100

    phone nav fix

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,17 @@
 const hamburger = document.querySelector("#hamburger");
 const popout = document.querySelector(".popout");
+const heading = document.querySelector("#jonathan");
 
 hamburger.addEventListener("click", () => {
+    if (heading.style.display == "none") {
+        heading.style.display = "block";
+        popout.classList.add("hidden");
+        return
+    }
+
+    if (window.innerHeight < 600) {
+        heading.style.display = "none";
+    }
+
     popout.classList.toggle("hidden");
 })
